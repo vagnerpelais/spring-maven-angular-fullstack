a51bf31b1f5087ff8cd05bf151e7068b0c24d8c3
vagner bohm <vagnerpelais@gmail.com>
2023-07-21 14:59:19 -0300

    first commit, datasource  configurations and dependencies

---
+++ src/main/java/com/vagnerpelais/securecapita/SecurecapitaApplication.java
@@ -0,0 +1,13 @@
+package com.vagnerpelais.securecapita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurecapitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurecapitaApplication.class, args);
+	}
+
+}
