a7a861f8f4a7d38f53f07989fc1263a8ecba3984
vagner bohm <vagnerpelais@gmail.com>
2023-07-24 12:09:51 -0300

    added profiles, banner and rename main app

--- src/main/java/com/vagnerpelais/securecapita/SecureCapitaApplication.java
+++ src/main/java/com/vagnerpelais/securecapita/SecureCapitaApplication.java
@@ -2,17 +2,16 @@ package com.vagnerpelais.securecapita;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class SecurecapitaApplication {
+@SpringBootApplication()
+public class SecureCapitaApplication {
 	private static final Integer STRENGTH = 12;
 
 	public static void main(String[] args) {
-		SpringApplication.run(SecurecapitaApplication.class, args);
+		SpringApplication.run(SecureCapitaApplication.class, args);
 	}
 
 	@Bean
